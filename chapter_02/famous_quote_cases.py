cc91d5a1a1e2bc3979af9ebe5eb4a82975e59ca8
jofronic <treewo@hotmail.com>
2025-07-05 17:33:50 -0400

    Update

--- chapter_02/famous_quote_cases.py
+++ chapter_02/famous_quote_cases.py
@@ -1,3 +1,4 @@
+# using print(f) to format string variables
 first_name= 'albert'
 last_name = 'eintstein'
 print(f"{first_name.title()} {last_name.title()} once said, a person who never made a mistake never tried anything new")
