65d8dafdd3abc209e9adf6ff8ee543e4e54c95a8
jofronic <treewo@hotmail.com>
2025-07-07 22:12:59 -0400

    chapter 4

--- chapter_04/magicians.py
+++ chapter_04/magicians.py
@@ -16,7 +16,7 @@ for magician in magicians:
 #print a message to each index
 magicians = ['alice','david','carolina']
 for magician in magicians:
-    print(f'{magician.title()}, that was a great trick\t')
+    print(f'{magician.title()}, that was a great trick')
     print(f"I can't wait to see you next trick, {magician.title()}\n")
 
 print(f'Thank you everyone. That was great magic show')
