8f72843fb18c3e694f2e73072c57856987b511ce
jofronic <treewo@hotmail.com>
2025-07-08 00:59:47 -0400

    updates

--- animals.py
+++
@@ -1,5 +0,0 @@
-animals = ['tiger', 'bear', 'cow']
-for mammals in animals:
-    print(f"{mammals.title()} are not great pets")
-
-print(f"\nThese animals cant be together in a room.")
--- chapter_04/buffet.py
+++ chapter_04/buffet.py
@@ -1,10 +1,10 @@
 buffet = ('Chicken', 'Beef', 'Fish', 'Hot Dog', 'Ice Cream', 'Soda')
 for food in buffet: 
-    print(buffet)
+    print(food.title())
 
 # buffet.append('bear')
 print('')
 
 buffet = ('Liver', 'Pork', 'Fish', 'hot dog', 'ice cream', 'soda')
-for fodd in buffet:
-    print(buffet)
+for food in buffet:
+    print(food.title())
