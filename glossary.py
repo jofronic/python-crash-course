95ddb9362acdfa3eeb0472cb2439d3b7e24bfdc9
jofronic <treewo@hotmail.com>
2025-10-05 23:38:34 -0400

    update

---
+++ glossary.py
@@ -0,0 +1,19 @@
+glossary_0 = {'variable' : 'a named container that stores a value in memory.',
+              'string' : 'a string is a sequence of characters inside quotes.',
+               'list' : 'a collection that can store multiple items in one variable.' ,
+               'function' : 'a reusable block of code that performs a specific task.',
+              'if_statement' : 'used to make decisions — only runs code if a condition is true.' }
+
+variable_00 = glossary_0['variable']
+variable_01 = glossary_0['string']
+variable_02 = glossary_0['list']
+variable_03 = glossary_0['function']
+variable_04 = glossary_0['if_statement']
+
+
+
+print(f" A variable is {variable_00}")
+print(f" A string is {variable_01}")
+print(f" A list is {variable_02}")
+print(f" A function is {variable_03}")
+print(f" An if statement is {variable_04}")
---
+++ peron.py
@@ -0,0 +1,10 @@
+person_0 = {'First Name': 'Cecil',
+             'Last Name': 'Oye', 
+             'Age':'39',
+             'City':'NJ'}
+
+print(person_0['First Name'])
+print(person_0['Last Name'])
+print(person_0['Age'])
+print(person_0['City'])
+
