264f3a246c663ce9a7c6937a3652a44f2fd273cf
jofronic <treewo@hotmail.com>
2025-11-04 00:52:08 -0500

    update

--- chapter_07/dream_vacations.py
+++ chapter_07/dream_vacations.py
@@ -1,6 +1,23 @@
-country = input('If you could visit one place in the world where would you go? ')
 visited  = []
-answwer  = input(country(visited))
-for places in visited:
+stop = 'quit'
+while True:
+    country = input("If you could visit one place in the world where would you go? (Type 'quit' to stop program) ")
+    if country.lower() == stop:
+        break      
     
+    else:
+        visited.append(country)   
+        print(f"{country.title()} that sounds nice, lets check the flights!")
+        print("Tell me another place you'd like to visit")
+
+
+if not visited:
+       print("Im sorry you dont want to travel.")
+  
+else:
+    print(f"You would like to visit:")
+    for places in visited:   
+        
+        print(places.title())
+      
 
