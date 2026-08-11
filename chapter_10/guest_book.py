3d71cd7156aee8bc55e025584ef494cd75ce4b87
jofronic <treewo@hotmail.com>
2026-08-11 03:57:54 -0400

    updates

--- chapter_10/guest_book.py
+++ chapter_10/guest_book.py
@@ -3,7 +3,7 @@ from pathlib import Path
 response = ''
 content = input("What is your name? Press q to stop program\n")
 while content != 'q':    
-    response += content+"\n"
+    response += content +"\n"
     content = input("What is your name? Press q to stop program\n")
 
 path = Path('Guest_book.txt')
